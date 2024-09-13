0ddea090fe1f217010aad35906147ebd6e30f50c
ferzian <m.ferzian09@gmail.com>
2024-09-13 21:06:17 +0700

    first commit

---
+++ first.js
@@ -0,0 +1,28 @@
+const age = 20
+const name = 'Ferzian'
+console.log(`Saya ${name}, umur saya ${age} tahun`);
+console.log('Hallo, saya ' + name + '!. Umur saya ' + age + '.');
+
+console.log();
+
+// dalam 1 baris terdapat 3 statement/instruksi
+let agee = 20; const namee = 'Ferziann'; agee = 18; console.log(`Saya ${namee}, umur saya ${agee} tahun`);
+
+console.log();
+
+// backtik sebagai template literals
+const currentDay = new Date().getDay();
+const currentMonth = new Date().getMonth();
+const currentYear = new Date().getFullYear();
+const text = `Sekarang adalah tanggal ${currentDay} - ${currentMonth} - ${currentYear}.`;
+console.log(text);
+
+console.log()
+
+// tipe data number
+const result = 50 / 0;
+console.log(result);
+const resultt = Number('Dicoding');
+console.log(resultt);
+const apakek = Number ('20.060')
+console.log(apakek);
