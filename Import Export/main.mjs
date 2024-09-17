a7426a28337c31ebcaaa79bb5c963544ee7f3519
ferzian <m.ferzian09@gmail.com>
2024-09-17 10:58:48 +0700

    Quiz Done

--- Import Export/main.mjs
+++ Import Export/main.mjs
@@ -1,48 +1,75 @@
 // default import
-import result from './anotherfile.mjs';
+import result from "./anotherfile.mjs";
 
 result();
 
-console.log()
+console.log();
 
 // named import
-import { myyFunction } from './anotherfile.mjs';
+import { myyFunction } from "./anotherfile.mjs";
 
 myyFunction();
 
-console.log()
+console.log();
 
 // import satu-satu (termasuk named import)
-import { name, email, age } from './anotherfile.mjs';
+import { name, email, age } from "./anotherfile.mjs";
 
 console.log(name);
 console.log(email);
 console.log(age);
 
-console.log()
+console.log();
 
 // import semua
-import * as variable from './anotherfile.mjs';
+import * as variable from "./anotherfile.mjs";
 
 console.log(variable.name);
 console.log(variable.email);
 console.log(variable.age);
 
-console.log()
+console.log();
 
 // import dari 2 modul
-import { myFunction as userFunction } from './user.mjs';
-import { myFunction as customerFunction } from './customer.mjs';
+import { myFunction as userFunction } from "./user.mjs";
+import { myFunction as customerFunction } from "./customer.mjs";
 
 userFunction();
 customerFunction();
 
-console.log()
-console.log('===========================================')
+console.log();
+console.log("===========================================");
 
 // import variabel dan function
-import * as user from './anotherfile.mjs';
- 
+import * as user from "./anotherfile.mjs";
+
 console.log(user.namee);
 console.log(user.favoriteFood);
 user.sayHi(user.namee);
+
+console.log();
+console.log("===========================================");
+
+// quiz
+/**
+ * TODO:
+ * 1. Impor fungsi unique dari utils.mjs secara default
+ * 2. Impor fungsi splitString dari utils.mjs secara named
+ *
+ * Catatan.
+ * Jangan ubah kode apa pun yang sudah tersedia sebelumnya
+ */
+
+import unique from "./utils.mjs";
+unique();
+
+import { splitString } from "./utils.mjs";
+
+const string = "saippuakivikauppias";
+const array = splitString(string);
+const uniqueArray = unique(array);
+
+console.log(string);
+console.log(array);
+console.log(uniqueArray);
+    
