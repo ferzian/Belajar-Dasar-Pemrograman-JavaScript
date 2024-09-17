e705acaa440302c7996e8d9b50e81fb1c385350c
ferzian <m.ferzian09@gmail.com>
2024-09-17 14:30:33 +0700

    Encapsulation

--- oop.js
+++ oop.js
@@ -1,64 +1,134 @@
 // ES6 class (constructor function)
 class Person {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
-    }
-  
-    eat() {
-      console.log(`${this.name} is eating`);
-    }
-    
-    identity(){
-      console.log(`${this.name} is ${this.age} years old`);
-    }
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
   }
-  
-  // Membuat instance dari Person
-  const person1 = new Person('Alice', 30);
-  const person2 = new Person('Bob', 25);
-  
-  console.log(person1.name); // Output: Alice
-  console.log(person2.name); // Output: Bob
-  
-  person1.eat(); // Output: Alice is eating
-  person2.eat(); // Output: Bob is eating
-  person1.identity();
 
+  eat() {
+    console.log(`${this.name} is eating`);
+  }
 
-  console.log()
-  console.log('======================================')
+  identity() {
+    console.log(`${this.name} is ${this.age} years old`);
+  }
+}
 
-  // inheritance (pewarisan)
-  class SmartPhones {
-    constructor(color, brand, model) {
-      this.color = color;
-      this.brand = brand;
-      this.model = model;
-    }
-  
-    charging() {
-      console.log(`Charging ${this.model}`);
-    }
+// Membuat instance dari Person
+const person1 = new Person("Alice", 30);
+const person2 = new Person("Bob", 25);
+
+console.log(person1.name); // Output: Alice
+console.log(person2.name); // Output: Bob
+
+person1.eat(); // Output: Alice is eating
+person2.eat(); // Output: Bob is eating
+person1.identity();
+
+console.log();
+console.log("======================================");
+
+// inheritance (pewarisan)
+class SmartPhones {
+  constructor(color, brand, model) {
+    this.color = color;
+    this.brand = brand;
+    this.model = model;
   }
   
-  class iOS extends SmartPhones {
-    airDrop() {
-      console.log('iOS have a behavior AirDrop');
-    }
+  charging() {
+    console.log(`Charging ${this.model}`);
+  }
+}
+
+class iOS extends SmartPhones {
+  airDrop() {
+    console.log("iOS have a behavior AirDrop");
+  }
+}
+
+class Android extends SmartPhones {
+  splitScreen() {
+    console.log("Android have a Split Screen");
+  }
+}
+
+const ios = new iOS("black", "A", "12 Pro Max");
+const android = new Android("white", "B", "Galaxy S21");
+
+ios.charging(); // Output: Charging 12 Pro Max
+ios.airDrop(); // Output: iOS have a behavior AirDrop
+
+android.charging(); // Output: Charging Galaxy S21
+android.splitScreen(); // Output: Android have a Split Screen
+
+console.log();
+console.log("======================================");
+
+// encapsulation
+class CoffeeMachine {
+  constructor(waterAmount) {
+    this.waterAmount = waterAmount;
+    this._temperature = 90;
   }
   
-  class Android extends SmartPhones {
-    splitScreen() {
-      console.log('Android have a Split Screen');
-    }
+  set temperature(temperature) {
+    console.log("you are not allowed to change the temperature");
   }
   
-  const ios = new iOS('black', 'A', '12 Pro Max');
-  const android = new Android('white', 'B', 'Galaxy S21');
+  get temperature() {
+    return this._temperature;
+  }
+}
+
+const coffee = new CoffeeMachine(10);
+console.log("Sebelum diubah: ", coffee.temperature);
+coffee.temperature = 100;
+console.log("Setelah diubah: ", coffee.temperature);
+
+// nilai tidak dapat diubah (encapsulation)
+class CoffeeMachine {
+  #temperature = 90;
   
-  ios.charging(); // Output: Charging 12 Pro Max
-  ios.airDrop(); // Output: iOS have a behavior AirDrop
+  constructor(waterAmount) {
+    this.waterAmount = waterAmount;
+    this.#temperature = this.#defaultTemperature();
+  }
   
-  android.charging(); // Output: Charging Galaxy S21
-  android.splitScreen(); // Output: Android have a Split Screen
+  set temperature(temperature) {
+    console.log("you are not allowed to change the temperature");
+  }
+  
+  get temperature() {
+    return this.#temperature;
+  }
+  
+  #defaultTemperature() {
+    return 90;
+  }
+}
+
+console.log();
+console.log("======================================");
+
+// nilai tidak dapat diubah (encapsulation)
+class CoffeeMachine {
+  #temperature = 90;
+
+  constructor(waterAmount) {
+    this.waterAmount = waterAmount;
+    this.#temperature = this.#defaultTemperature();
+  }
+
+  set temperature(temperature) {
+    console.log('you are not allowed to change the temperature');
+  }
+
+  get temperature() {
+    return this.#temperature;
+  }
+
+  #defaultTemperature() {
+    return 90;
+  }
+}
